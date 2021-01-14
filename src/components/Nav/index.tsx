c02f746fc0da2de6dfc1349306b94acb8505dde6
Bubo Dev <dev@bubo.io>
2021-01-14 13:17:20 -0600

    Se cambio el logo

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Nav, ConnectorId } from '@pancakeswap-libs/uikit'
 import { useWeb3React } from '@web3-react/core'
 import { allLanguages } from 'constants/localisation/languageCodes'
@@ -11,6 +11,20 @@ const Menu = () => {
   const { account, activate, deactivate } = useWeb3React()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
+
+  useEffect(() => {
+    const checkLogo = () => {
+      const label = document.querySelector('[aria-label="Pancake home page"]')
+      if (label !== null) {
+        label.innerHTML = '<img src="/images/logo@2x.png" />'
+        label.setAttribute('aria-label', 'Swap Dex')
+        setTimeout(() => {
+          checkLogo()
+        }, 100)
+      }
+    }
+    checkLogo()
+  }, [])
 
   return (
     <Nav
