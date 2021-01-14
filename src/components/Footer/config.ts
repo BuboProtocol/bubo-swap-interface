0c4b311ffa0bb9ecabce51b2d0c69a386d474573
Bubo Dev <dev@bubo.io>
2021-01-13 18:52:52 -0600

    Se modifico el footer

---
+++ src/components/Footer/config.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    label: 'Github',
+    href: 'https://github.com/buboprotocol'
+  },
+  {
+    label: 'Blog',
+    href: 'https://buboprotocol.medium.com'
+  },
+  {
+    label: 'Telegram',
+    href: 'https://t.me/buboprotocol'
+  },
+  {
+    label: 'Twitter',
+    href: 'https://twitter.com/buboprotocol'
+  }
+]
